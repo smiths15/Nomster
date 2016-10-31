b4a6b4712dc520f26e0dcbfb86dade49a7563a44
Smith Saintil <smith.saintil@gmail.com>
2016-10-31 09:10:38 +0000

    added user id column and connected the user id to the places table via user activerecord and places active record

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -10,7 +10,7 @@ before_action :authenticate_user!, only: [:new, :create]
   end
   
   def create
-    Place.create(place_params)
+    current_user.places.create(place_params)
     redirect_to root_path
   end 
  
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,5 @@
 class Place < ActiveRecord::Base
+  belongs_to :user
   def self.search(search)
     self.where("name ILIKE ? OR address ILIKE ? or description ILIKE ?", "%#{search}%", "%#{search}%", "%#{search}%")
   end
