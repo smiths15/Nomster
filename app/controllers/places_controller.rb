dd85b470b34a1117746414c0b83c6e357924be77
Smith Saintil <smith.saintil@gmail.com>
2016-11-03 10:37:24 +0000

    blocked users who arre not signed in and who didn't create a place from editing, updating, or destroying a place

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-before_action :authenticate_user!, only: [:new, :create]  
+before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]  
 
   def index
     @places = Place.all.page(params[:page]).per(5)
@@ -32,14 +32,33 @@ before_action :authenticate_user!, only: [:new, :create]
 
   def edit
     @place = Place.find(params[:id])
+
+    if @place.user != current_user
+      return render text: 'Not allowed', layout: true, status: :forbidden 
+    end
   end
 
   def update
     @place = Place.find(params[:id])
+    
+    if @place.user != current_user
+      return render text: 'Not allowed', layout: true, status: :forbidden 
+    end
+
     @place.update_attributes(place_params)
     redirect_to root_path
   end
 
+def destroy
+  @place = Place.find(params[:id])
+  
+  if @place.user != current_user
+    return render text: 'Not allowed', layout: true, status: :forbidden 
+  end
+
+  @place.destroy
+  redirect_to root_path
+end
 
   private
 
