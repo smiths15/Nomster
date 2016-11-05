e7da74ee78970d2f75485bbfb7cdf1efe974076a
Smith Saintil <smith.saintil@gmail.com>
2016-11-05 17:00:11 +0000

    created validation for name and styled the error field  with a pink box

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,6 +1,6 @@
 class Place < ActiveRecord::Base
   belongs_to :user
-  validates :name, presence: true
+  validates :name, :address, :description, presence: true
   def self.search(search)
     self.where("name ILIKE ? OR address ILIKE ? or description ILIKE ?", "%#{search}%", "%#{search}%", "%#{search}%")
   end
