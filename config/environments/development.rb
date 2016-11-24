c5e53c7d64ba52a29e4abb7ae2223900653004bf
Smith Saintil <smith.saintil@gmail.com>
2016-11-24 00:14:54 +0000

    commented out development email and went back to test method

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,17 +1,19 @@
 Rails.application.configure do
   
-  #config for gmail account
-  config.action_mailer.delivery_method = :smtp 
+  config.action_mailer.delivery_method = :test
 
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "gmail.com",
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV['GMAIL_ADDRESS'],
-    password: ENV['GMAIL_PASSWORD']
-  }
+  #config for gmail account
+  #config.action_mailer.delivery_method = :smtp 
+
+  #config.action_mailer.smtp_settings = {
+    #address: "smtp.gmail.com",
+    #port: 587,
+    #domain: "gmail.com",
+    #authentication: "plain",
+    #enable_starttls_auto: true,
+    #user_name: ENV['GMAIL_ADDRESS'],
+    #password: ENV['GMAIL_PASSWORD']
+  # }
 
   # Settings specified here will take precedence over those in config/application.rb.
 
