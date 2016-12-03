f061326f2460e1f3cf060d5d00a0b0c9df0bd087
Smith Saintil <smith.saintil@gmail.com>
2016-12-03 16:11:44 +0000

    crated users controller and added users path on routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
 
   get 'search', to: 'places#search'
 
+  resources :users, only: :show
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
